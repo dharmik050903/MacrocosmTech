2fcd02044436d50a7b941f597d93829790e9c081
Dharmik <Dharmiksuthar0509@gmai.com>
2025-11-17 23:19:15 +0530

    Header fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      screens: {
+        'xs': '475px',
+      },
       colors: {
         "primary": "#256af4",
         "background-light": "#f5f6f8",
